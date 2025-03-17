65bacf2a00044cfb03ccc40f67a3ef8c4288b00b
Alfrerose12 <20233L001112@utcv.edu.mx>
2025-03-17 16:02:36 -0600

    Feat: Modificaciones de la app

---
+++ app-social/src/app/login/login.page.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+  standalone: false
+})
+export class LoginPage implements OnInit {
+
+  email: string = '';
+  password: string = '';
+  constructor(private authService: AuthService, private router: Router) { }
+
+  ngOnInit() {
+  }
+  doLogin() {
+    this.authService.login(this.email, this.password).then(() => {
+      this.router.navigate(['/home']);
+    }).catch((err: Error) => {
+      alert('Los datos son incorrectos o no existe el usuario');
+    });
+  }
+  loginFacebook() {
+    alert('funciona')
+}  
+}
--- app-social/src/app/tabs/tabs-routing.module.ts
+++ app-social/src/app/tabs/tabs-routing.module.ts
@@ -8,27 +8,27 @@ const routes: Routes = [
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
-        loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
+        path: 'inicio',
+        loadChildren: () => import('../inicio/inicio.module').then( m => m.InicioPageModule)
       },
       {
-        path: 'tab2',
-        loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
+        path: 'video',
+        loadChildren: () => import('../video/video.module').then( m => m.VideoPageModule)
       },
       {
-        path: 'tab3',
-        loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
+        path: 'amigos',
+        loadChildren: () => import('../amigos/amigos.module').then( m => m.AmigosPageModule)
       },
       {
         path: '',
-        redirectTo: '/tabs/tab1',
+        redirectTo: '/tabs/inicio',
         pathMatch: 'full'
       }
     ]
   },
   {
     path: '',
-    redirectTo: '/tabs/tab1',
+    redirectTo: '/login',
     pathMatch: 'full'
   }
 ];
