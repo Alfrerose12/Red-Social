c9b67acf1feeb6182b2b232ba284a541740fd1d2
Alfrerose12 <20233L001112@utcv.edu.mx>
2025-03-17 16:04:27 -0600

    l

--- app-social/src/app/login/login.page.ts
+++ app-social/src/app/login/login.page.ts
@@ -11,17 +11,17 @@ export class LoginPage implements OnInit {
 
   email: string = '';
   password: string = '';
-  constructor(private authService: AuthService, private router: Router) { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
-  doLogin() {
-    this.authService.login(this.email, this.password).then(() => {
-      this.router.navigate(['/home']);
-    }).catch((err: Error) => {
-      alert('Los datos son incorrectos o no existe el usuario');
-    });
-  }
+  // doLogin() {
+  //   this.authService.login(this.email, this.password).then(() => {
+  //     this.router.navigate(['/home']);
+  //   }).catch((err: Error) => {
+  //     alert('Los datos son incorrectos o no existe el usuario');
+  //   });
+  // }
   loginFacebook() {
     alert('funciona')
 }  
