a3df70c2100404611f8499edf0d4f4a1778edf17
Alfrerose12 <20233L001112@utcv.edu.mx>
2025-03-18 10:35:23 -0600

    Feat: Correcciones en el ts de amigos y correcciones en el card-post correspondiente

--- app-social/src/app/amigos/amigos.page.ts
+++ app-social/src/app/amigos/amigos.page.ts
@@ -16,28 +16,28 @@ export class AmigosPage {
   posts = [
     { 
       id: 1, 
-      user: 'Daniel', 
+      user: 'Daniel García Cobos', 
       userAvatar: 'https://scontent-qro1-1.xx.fbcdn.net/v/t39.30808-6/431867500_1436737523623964_7976501891398235389_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeEpxIk1J46SiwvOtOHYkyHLSDp5C_fc9LNIOnkL99z0swmxxXH1BvwllNv1gUQNvqDjAYjnMz2NuMUK-5b9mVd1&_nc_ohc=5B5DIFgEk88Q7kNvgHmq3Ag&_nc_oc=Adhq2DVp8j5tXjBJccwhVr3waPKV2MkDJcyYBGOndWWfmuDFL-nX3nq69w1RRCUtVs8yJUdhh9S_zy5b5aaGLBxA&_nc_zt=23&_nc_ht=scontent-qro1-1.xx&_nc_gid=L11vFi_N8pfnhrbDDk0Q2g&oh=00_AYF437NrQ2xNybVEOn4bP94y5x5k_W597v9NWdoK7x3d3w&oe=67DEF947', 
       content: 'https://scontent-qro1-2.xx.fbcdn.net/v/t39.30808-6/356408095_101628749655448_7418084051985274865_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeHjvb63jPyP-WLRUW9jsSb0ojnMwKo7Um6iOczAqjtSbspTM8PaNQd_3CfqItf_kboQI2Ed0NzShv3eT7A7sGmu&_nc_ohc=roa8p1bO0tQQ7kNvgGLeWGx&_nc_oc=AdhGwUG3aUDKKvAveSIzMtfs1qfiawyUExBDSGbbGS4JMEPXNf_cXL1TApnqWrXAQ5qBcpMs2XvD349XmJBaj31R&_nc_zt=23&_nc_ht=scontent-qro1-2.xx&_nc_gid=yFTrdd7xXYJKqL5ftMDa0w&oh=00_AYHtE6Hkt_8E1Q4ITYNgQSPRk-3nNHSl9Aeio2wVJWHACQ&oe=67DEDD25',
       type: 'image' 
     },
     { 
       id: 2, 
-      user: 'Karen Garcia', 
+      user: 'Karen García', 
       userAvatar:'https://scontent-qro1-2.xx.fbcdn.net/v/t39.30808-6/438205687_3749325638689853_6668461089632969725_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeF-k0WnzaIr4HBV8odXzRgKvPzA_2d0qOm8_MD_Z3So6SXiev4u4eSvniBvb4_4ovV6eEQdeSTzkqUXzS_M-IZ2&_nc_ohc=jexcdFRBzrwQ7kNvgGthOuX&_nc_oc=Adgg7Se42rgdOkrTcry1GI-RLmULfeJvOHDoAC-MLY9tl45OuoeW4DrPCBpudPAwDReFMCDG5HQhKyefXjfxC12n&_nc_zt=23&_nc_ht=scontent-qro1-2.xx&_nc_gid=nRTH58TLQfGZgGpjBB9fgg&oh=00_AYGey0h7oWKsyJETNvfKuiTsvZEiqqUXTgaz0TDlPQAK9g&oe=67DEDA7E', 
       content: 'https://scontent-qro1-2.xx.fbcdn.net/v/t39.30808-6/438205687_3749325638689853_6668461089632969725_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeF-k0WnzaIr4HBV8odXzRgKvPzA_2d0qOm8_MD_Z3So6SXiev4u4eSvniBvb4_4ovV6eEQdeSTzkqUXzS_M-IZ2&_nc_ohc=jexcdFRBzrwQ7kNvgGthOuX&_nc_oc=Adgg7Se42rgdOkrTcry1GI-RLmULfeJvOHDoAC-MLY9tl45OuoeW4DrPCBpudPAwDReFMCDG5HQhKyefXjfxC12n&_nc_zt=23&_nc_ht=scontent-qro1-2.xx&_nc_gid=nRTH58TLQfGZgGpjBB9fgg&oh=00_AYGey0h7oWKsyJETNvfKuiTsvZEiqqUXTgaz0TDlPQAK9g&oe=67DEDA7E',
       type: 'image'
     },
     { 
       id: 3, 
-      user: 'Alfrerose', 
+      user: 'Alfredo Rosas Espinosa', 
       userAvatar: 'https://scontent-qro1-2.xx.fbcdn.net/v/t39.30808-6/464615243_1886191385209243_7014231757131260969_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeFqrawxkOZ3AhIunq9Hg81-2iKzyz6-KuTaIrPLPr4q5BlWytGwDmSPHhvOZ7Vrwa3PEhVXVl_SSAzCTuazhmfr&_nc_ohc=sxgvqbJ2n1gQ7kNvgEV3KUu&_nc_oc=AdiQeOlPJJzzhW75nsGDEfvUpn6gUm9YG8mRzR09OSNkPNtwQeXyF8exJzsq3M16dLAdM5SllMU8Yqv8Upo2bPYG&_nc_zt=23&_nc_ht=scontent-qro1-2.xx&_nc_gid=vYnTM16GfDECzwm8YXJWxw&oh=00_AYGGgrB9XYkW4PMZjr4H3LV9kCcZJZ659JQ_uhuB8CiTtg&oe=67DEDEAE', 
       content: 'https://scontent-qro1-2.xx.fbcdn.net/v/t39.30808-6/464615243_1886191385209243_7014231757131260969_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeFqrawxkOZ3AhIunq9Hg81-2iKzyz6-KuTaIrPLPr4q5BlWytGwDmSPHhvOZ7Vrwa3PEhVXVl_SSAzCTuazhmfr&_nc_ohc=sxgvqbJ2n1gQ7kNvgEV3KUu&_nc_oc=AdiQeOlPJJzzhW75nsGDEfvUpn6gUm9YG8mRzR09OSNkPNtwQeXyF8exJzsq3M16dLAdM5SllMU8Yqv8Upo2bPYG&_nc_zt=23&_nc_ht=scontent-qro1-2.xx&_nc_gid=vYnTM16GfDECzwm8YXJWxw&oh=00_AYGGgrB9XYkW4PMZjr4H3LV9kCcZJZ659JQ_uhuB8CiTtg&oe=67DEDEAE', 
       type: 'image' 
     },
     { 
       id: 4, 
-      user: 'Diegod', 
+      user: 'Diego Sánchez', 
       userAvatar: 'https://scontent-qro1-1.xx.fbcdn.net/v/t1.6435-9/116348131_1504688923073071_730660850520240891_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeGaneIbaLYQ78j2wqQsQ6nBfwIfOnA9sgV_Ah86cD2yBbzYe9ZyDQFu4zHgscLwwH8seSzGz5wy4wlRnLJyjjWf&_nc_ohc=EakWhDyHKlkQ7kNvgHqc3ti&_nc_oc=AdixyJpWbzkPLIzDuZYeaxWkXaKS-t9D-LG-4mftaXb3J4DZ9VQBFjSzxNeoDZDnX8E&_nc_zt=23&_nc_ht=scontent-qro1-1.xx&_nc_gid=YR256IX3QStS7Kio-W_5gw&oh=00_AYGetA4D8ua2rRCIVL0wu2TQG5sTWjtMRMoEZVaRQtEfOQ&oe=6800EB7F', 
       content: 'https://scontent-qro1-2.xx.fbcdn.net/v/t39.30808-6/464615243_1886191385209243_7014231757131260969_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeFqrawxkOZ3AhIunq9Hg81-2iKzyz6-KuTaIrPLPr4q5BlWytGwDmSPHhvOZ7Vrwa3PEhVXVl_SSAzCTuazhmfr&_nc_ohc=sxgvqbJ2n1gQ7kNvgEV3KUu&_nc_oc=AdiQeOlPJJzzhW75nsGDEfvUpn6gUm9YG8mRzR09OSNkPNtwQeXyF8exJzsq3M16dLAdM5SllMU8Yqv8Upo2bPYG&_nc_zt=23&_nc_ht=scontent-qro1-2.xx&_nc_gid=vYnTM16GfDECzwm8YXJWxw&oh=00_AYGGgrB9XYkW4PMZjr4H3LV9kCcZJZ659JQ_uhuB8CiTtg&oe=67DEDEAE', 
       type: 'image' 
@@ -58,7 +58,7 @@ export class AmigosPage {
     },
     { 
       id: 7, 
-      user: 'Jhonatan Beltran', 
+      user: 'Yonatan Beltrán', 
       userAvatar: 'https://scontent-qro1-1.xx.fbcdn.net/v/t39.30808-6/338436586_776509410796139_1581911395111579720_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeGUe5YRM3aFdryk0g6IEb52397sJ3Oun4vf3uwnc66fi35CPK_S0E4A9xMGkWQAOGFXouyXBSX88ipWcM-aLebj&_nc_ohc=qJ2ZgBjnBgYQ7kNvgHqfqb0&_nc_oc=AdiVxYGuf6UDfAXKKQVffZeKZB-TAWL_C0Mbt7qp_jna9swbbZ1SN13dE1JMZBCKXrk&_nc_zt=23&_nc_ht=scontent-qro1-1.xx&_nc_gid=xZ6dC9qr3_EotoXjQFSVKw&oh=00_AYHd8Rt94myImwKfZ9rNbu1DZRh6TlUj3XZy2GDjyZ5TLg&oe=67DF74BF', 
       content: 'https://scontent-qro1-2.xx.fbcdn.net/v/t39.30808-6/464615243_1886191385209243_7014231757131260969_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeFqrawxkOZ3AhIunq9Hg81-2iKzyz6-KuTaIrPLPr4q5BlWytGwDmSPHhvOZ7Vrwa3PEhVXVl_SSAzCTuazhmfr&_nc_ohc=sxgvqbJ2n1gQ7kNvgEV3KUu&_nc_oc=AdiQeOlPJJzzhW75nsGDEfvUpn6gUm9YG8mRzR09OSNkPNtwQeXyF8exJzsq3M16dLAdM5SllMU8Yqv8Upo2bPYG&_nc_zt=23&_nc_ht=scontent-qro1-2.xx&_nc_gid=vYnTM16GfDECzwm8YXJWxw&oh=00_AYGGgrB9XYkW4PMZjr4H3LV9kCcZJZ659JQ_uhuB8CiTtg&oe=67DEDEAE', 
       type: 'image' 
@@ -79,7 +79,7 @@ export class AmigosPage {
     },
     { 
       id: 10, 
-      user: 'Abiel Otiveros', 
+      user: 'Abiel Ontiveros', 
       userAvatar: 'https://scontent-qro1-2.xx.fbcdn.net/v/t39.30808-6/328239490_1010448317025329_6928287099750701897_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeHqp2HCinfegweJQFwHTO1owC-nY1kt0YrAL6djWS3RinfnCxcWMVwkXikchoBoCUqjEZCeeLONVwzgjeIfZA4o&_nc_ohc=_bHdGNX8DNYQ7kNvgGKQxvq&_nc_oc=Adj15O-uiiJTUe8CD-XeYy2zfJaYFoSNJmCypNWXoRvLew9rxo26cmP0HoSK0gSlQHY&_nc_zt=23&_nc_ht=scontent-qro1-2.xx&_nc_gid=GwNdWvLvaCIvkAYjBzjzlQ&oh=00_AYHKxLpB1HkYuMGHOmKmPcCkRtqI699-UxGmVDf_rGGcrg&oe=67DF5F0F', 
       content: 'https://scontent-qro1-2.xx.fbcdn.net/v/t39.30808-6/464615243_1886191385209243_7014231757131260969_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeFqrawxkOZ3AhIunq9Hg81-2iKzyz6-KuTaIrPLPr4q5BlWytGwDmSPHhvOZ7Vrwa3PEhVXVl_SSAzCTuazhmfr&_nc_ohc=sxgvqbJ2n1gQ7kNvgEV3KUu&_nc_oc=AdiQeOlPJJzzhW75nsGDEfvUpn6gUm9YG8mRzR09OSNkPNtwQeXyF8exJzsq3M16dLAdM5SllMU8Yqv8Upo2bPYG&_nc_zt=23&_nc_ht=scontent-qro1-2.xx&_nc_gid=vYnTM16GfDECzwm8YXJWxw&oh=00_AYGGgrB9XYkW4PMZjr4H3LV9kCcZJZ659JQ_uhuB8CiTtg&oe=67DEDEAE', 
       type: 'image' 
