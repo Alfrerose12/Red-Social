558d7097a19b4fe952de8b9cbecc84a51face1b7
Alfrerose12 <20233L001112@utcv.edu.mx>
2025-03-17 23:35:19 -0600

    Feat: Correción de lógica del registro y login

--- app-social/src/app/login/login.page.ts
+++ app-social/src/app/login/login.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../services/api.service';
-import { NavController } from '@ionic/angular';
+import { AlertController, NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -13,24 +13,36 @@ export class LoginPage implements OnInit {
   email: string = '';
   password: string = '';
 
-  constructor(private apiService: ApiService, private navController: NavController) { }
+  constructor(private apiService: ApiService, private navController: NavController, private alertController: AlertController) { }
 
   ngOnInit() {
   }
 
-  sesion() {
-    const credentials = {
-      email: this.email,
-      password: this.password
-    };
-    this.apiService.login(credentials).subscribe((response) => {
+  async sesion() {
+    if (!this.email || !this.password) {
+      this.mostrarAlerta('Error', 'Todos los campos son obligatorios');
+      return;
+    }
+
+    this.apiService.login({ email: this.email, password: this.password }).subscribe((response) => {
       console.log(response);
+      this.mostrarAlerta('Éxito', 'Iniciaste sesión correctamente');
       this.navController.navigateForward('/tabs/inicio');
     },
-      (error) => {
-        console.log('Error en el login:', error);
+      async (error) => {
+        console.log('Error en el registro: ', error);
+        this.mostrarAlerta('Error', 'Error al iniciar sesión')
       }
     );
+  }
+  
+  async mostrarAlerta(titulo: string, mensaje: string) {
+    const alert = await this.alertController.create({
+      header: titulo,
+      message: mensaje,
+      buttons: ['Cerrar']
+    });
+    await alert.present();
   }
 
   sesionGoogle() {
