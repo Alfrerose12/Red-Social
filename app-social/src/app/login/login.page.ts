6d99a7ca76d14bf5dd89288aad0bb5de322af1ea
diego <diegosanchezs777@gmail.com>
2025-03-17 23:36:23 -0600

    Feat: implementando API para facebook ya sirve logeandote

--- app-social/src/app/login/login.page.ts
+++ app-social/src/app/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ApiService } from '../services/api.service';
+import { FacebookLogin } from '@capacitor-community/facebook-login';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,17 +10,43 @@ import { ApiService } from '../services/api.service';
 })
 export class LoginPage implements OnInit {
 
-  constructor(private apiService: ApiService) { }
+  constructor(private router: Router) {}
 
   ngOnInit() {
+    // Inicializar FacebookLogin en Capacitor (si no lo has hecho antes)
+    FacebookLogin.initialize({ appId: '1174025107505497' });
   }
 
-  sesion() {
+  async sesionFacebook() {
+    try {
+      // Solicita el login con Facebook
+      const response = await FacebookLogin.login({ permissions: ['email', 'public_profile'] });
+
+      if (response.accessToken) {
+        console.log('Token de acceso:', response.accessToken.token);
+        this.router.navigate(['/tabs/inicio']); // Redirigir después del login
+      } else {
+        console.log('Inicio de sesión cancelado.');
+      }
+    } catch (error) {
+      console.error('Error al iniciar sesión con Facebook:', error);
+    }
+  }
+
+  async cerrarSesionFacebook() {
+    try {
+      // Cerrar sesión con Facebook
+      await FacebookLogin.logout();
+      console.log('Sesión de Facebook cerrada');
+    } catch (error) {
+      console.error('Error al cerrar sesión de Facebook:', error);
+    }
+  }
+  sesion(){
     
   }
 
   sesionGoogle() {
     window.location.href = 'http://localhost:3003/api/auth/google';
   }
-
 }
