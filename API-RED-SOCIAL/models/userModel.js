5fd1758c5ac9e203f96e22c024413ecce0740eea
Alfrerose12 <20233L001112@utcv.edu.mx>
2025-03-20 08:38:24 -0600

    Fix: Modificando el login de la aplicación

--- API-RED-SOCIAL/models/userModel.js
+++ API-RED-SOCIAL/models/userModel.js
@@ -3,10 +3,14 @@ const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, unique: true, required: true },
-  password: { type: String, required: true },
-  provider: { type: String, default: 'local' }, // Indica si es local o de un proveedor como Google/Facebook
-  profilePicture: { type: String, default: 'https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y' }, // Foto de perfil predeterminada
+  password: { 
+    type: String, 
+    required: function() { return this.provider === 'local'; } 
+  },
+  provider: { type: String, enum: ['local', 'google'], default: 'local' },
+  googleId: { type: String, unique: true, sparse: true }, 
+  profilePicture: { type: String, default: 'https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y' },
   createdAt: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model('User', UserSchema);  
